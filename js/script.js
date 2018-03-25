c3a56905aa838f9ea000107cdb7517d21d12ff7f
Jakub Kilar <kuba.kilar@gmail.com>
2018-03-25 17:49:00 +0200

    finish seach countries engine

---
+++ js/script.js
@@ -0,0 +1,28 @@
+$(function() {
+
+  const url = "https://restcountries.eu/rest/v2/name/";
+  const countriesList = $('#countires');
+
+  function searchCountries() {
+    let countryName = $('#country-name').val(); // The .val() method is primarily used to get the values of form elements such as input, select and textarea.
+    if(!countryName.length) {
+      countryName = 'Poland';
+    }
+    // wykonuje asynchronous HTTP (Ajax) request
+    $.ajax({
+      url: url + countryName,
+      method: 'GET',
+      success: showCountriesList(), // podaje co ma się wykonać gdy serwer odpowie prawidłowo
+    })
+  }
+
+  function showCountriesList(resp) { // resp - obiekt JSON, który przesyła do niej metoda .ajax() - czyli co?
+    countriesList.empty();
+    resp.forEach(function(item) {
+      $('<li>').text(item.name).appendTo(countriesList);
+    });
+  }
+
+  $('search').click(searchCountries);
+
+}); // end document ready
